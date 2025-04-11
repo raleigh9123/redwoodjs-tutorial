8e97b45d53687826008ab897dfbb25a5b8c15bb3
Joseph Butterfield <raleigh9123@gmail.com>
2025-04-11 14:43:14 -0700

    Remove new user creation. Add TailwindCSS to project

--- api/src/functions/auth.ts
+++ api/src/functions/auth.ts
@@ -131,14 +131,15 @@ export const handler = async (
       salt,
       userAttributes: _userAttributes,
     }) => {
-      return db.user.create({
-        data: {
-          email: username,
-          hashedPassword: hashedPassword,
-          salt: salt,
-          // name: userAttributes.name
-        },
-      })
+      false
+      // return db.user.create({
+      //   data: {
+      //     email: username,
+      //     hashedPassword: hashedPassword,
+      //     salt: salt,
+      //     // name: userAttributes.name
+      //   },
+      // })
     },
 
     // Include any format checks for password here. Return `true` if the
