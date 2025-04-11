207fe27c681634bfc35274accab3f96cea87d0da
Joseph Butterfield <raleigh9123@gmail.com>
2025-04-11 14:09:45 -0700

    Initialize Redwood Tutorial App

---
+++ web/src/components/Article/Article.tsx
@@ -0,0 +1,21 @@
+import type { Post } from 'types/graphql'
+
+import { Link, routes } from '@redwoodjs/router'
+
+interface Props {
+  article: Post
+}
+
+const Article = ({ article }: Props) => {
+  return (
+    <article key={article.id}>
+      <h2>
+        <Link to={routes.article({ id: article.id })}>{article.title}</Link>
+      </h2>
+      <p>{article.body}</p>
+      <div>Posted at: {article.createdAt}</div>
+    </article>
+  )
+}
+
+export default Article
---
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -0,0 +1,44 @@
+import type { ArticlesQuery, ArticlesQueryVariables } from 'types/graphql'
+
+import { Link, routes } from '@redwoodjs/router'
+import type {
+  CellSuccessProps,
+  CellFailureProps,
+  TypedDocumentNode,
+} from '@redwoodjs/web'
+
+import Article from 'src/components/Article'
+
+export const QUERY: TypedDocumentNode<ArticlesQuery, ArticlesQueryVariables> =
+  gql`
+    query ArticlesQuery {
+      articles: posts {
+        id
+        title
+        body
+        createdAt
+      }
+    }
+  `
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({
+  error,
+}: CellFailureProps<ArticlesQueryVariables>) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({
+  articles,
+}: CellSuccessProps<ArticlesQuery, ArticlesQueryVariables>) => {
+  return (
+    <ul>
+      {articles.map((article) => (
+        <Article article={article} key={article.id} />
+      ))}
+    </ul>
+  )
+}
